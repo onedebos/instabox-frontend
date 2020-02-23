1571978e93281e8d3550b146b5088ea475d580b5
Adebola <onedebos@gmail.com>
2020-02-24 00:55:39 +0100

    change API to production

--- src/components/helpers/apiHelper.js
+++ src/components/helpers/apiHelper.js
@@ -1,4 +1,4 @@
-// const API_URL = "https://instabox-api.herokuapp.com";
-const API_URL = "http://localhost:3001";
+const API_URL = "https://instabox-api.herokuapp.com";
+// const API_URL = "http://localhost:3001";
 
 export default API_URL;
